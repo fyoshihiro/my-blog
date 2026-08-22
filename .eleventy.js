5001c0f4123f1f371843f2fb96ecfc3eb00efa45
fyoshihiro <93437543+fyoshihiro@users.noreply.github.com>
2026-08-22 15:53:54 +0900

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addCollection("post", function(collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md");
   });
