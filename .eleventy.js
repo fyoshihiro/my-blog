61646393131b2d287cb26edfd5d80b1c6666704d
fyoshihiro <93437543+fyoshihiro@users.noreply.github.com>
2026-08-22 16:14:49 +0900

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,18 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob("posts/*.md");
   });
 
+  // カテゴリごとの記事一覧を作る
+  eleventyConfig.addCollection("categories", function(collectionApi) {
+    const posts = collectionApi.getFilteredByGlob("posts/*.md");
+    const categories = {};
+    posts.forEach(post => {
+      const cat = post.data.category || "未分類";
+      if (!categories[cat]) categories[cat] = [];
+      categories[cat].push(post);
+    });
+    return categories;
+  });
+
   return {
     pathPrefix: "/blog/"
   };
