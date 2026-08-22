3d4ff12728a36a00168513bdff6e36a557c285dd
fyoshihiro <93437543+fyoshihiro@users.noreply.github.com>
2026-08-22 11:23:41 +0900

    Add posts list and individual post pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addCollection("post", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("posts/*.md");
+  });
 };
